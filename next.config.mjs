fcec488a2913e71b6494710a820495bbf229e015
Jack So <sojack@gmail.com>
2025-11-01 18:45:29 -0400

    general update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+ images: {
+    formats: ['image/avif', 'image/webp'],
+    qualities: [60, 75, 85, 100], // include 60 since something in your app requests it
+  },
+};
 
 export default nextConfig;
